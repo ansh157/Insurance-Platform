f42f54c46dd50b67eb194641b85b820c1c4d7290
Ansh Singh <anshsingh.x1234@gmail.com>
2026-02-27 14:27:55 +0530

    cookie changes

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -8,11 +8,11 @@ const generateToken = (res, userId) => {
   });
 
   res.cookie("jwt", token, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
-    maxAge: 7 * 24 * 60 * 60 * 1000,
-  });
+  httpOnly: true,
+  secure: true,
+  sameSite: "none",
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+});
 };
 
 // signup controller
